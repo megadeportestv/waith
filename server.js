e0e2dc2eb4e79a39a8ee34460a1a7722c83ed430
megadeportestv <123114938+megadeportestv@users.noreply.github.com>
2024-06-13 18:23:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,12 @@ cors_proxy.createServer({
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
+    // Custom header setting function
+    setHeaders: function (req, res, proxyRes) {
+      // Set custom Referer and Origin headers
+      proxyRes.headers['Referer'] = 'https://www.starplus.com; // Replace with your desired Referer
+      proxyRes.headers['Origin'] = 'https://www.starplus.com';  // Replace with your desired Origin
+    }
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
